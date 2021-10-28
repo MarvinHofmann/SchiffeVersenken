a108a91909ceea8b43f0ece707631aad780bed5d
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-10-28 10:39:39 +0200

    start

--- SchiffeVersenken/src/schiffeversenken/SchiffeVersenken.java
+++ SchiffeVersenken/src/schiffeversenken/SchiffeVersenken.java
@@ -19,7 +19,7 @@ public class SchiffeVersenken extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Hauptmenue.fxml"));
         
         Scene scene = new Scene(root);
         
