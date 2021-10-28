4ea2b0772b11bd43dd273ab72a195333492f4e6d
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-10-28 13:10:06 +0200

    ++

--- SchiffeVersenken/src/shapes/Schiff.java
+++ SchiffeVersenken/src/shapes/Schiff.java
@@ -7,45 +7,46 @@ package shapes;
 
 /**
  *
- * @author marvi 
- * GRUPPENNUMMER: 8 
+ * @author marvi GRUPPENNUMMER: 8
  */
 public abstract class Schiff {
+       
     protected int[] trefferArray;
 
     protected int groesse;
-   
-    /*
-    durchläuft Array und schaut ob getroffen 
-        1 -> treffer 
-        0 -> heil
-    Wenn alle alle getroffen dann versenkt
-    */
-    public boolean checkVersenkt(){
-        for(int i = 0; i<trefferArray.length; i++){
+
+    /**
+     * durchläuft Array und schaut ob getroffen 1 -> treffer 0 -> heil
+     *
+     * @return true für versenkt false für nicht versenkt
+     */
+    public boolean checkVersenkt() {
+        for (int i = 0; i < trefferArray.length; i++) {
             if (trefferArray[i] == 0) {
                 return false;
             }
         }
         return true;
     }
-    
-    /*
-    Aktualisiere Treffer Array an der jeweiligen Stelle
-    rufe danach check versenkt auf
-    BSP.:
-    
-        [][X][][] -> Stelle 1 getroffen wird mit 1 belegt
-        .
-        .
-        [X][X][X][X] -> Alle getroffen -> Array durchgehend mit eins belegt -> check versenkt gibt true zurück
-    
-    muss nicht überprüft werden, da nicht zwei mal auf die gleiche Stelle geschossen
-    werden kann
-    Woher kommt die Stelle wird hier geparsed nach row/col oder schon woanders?
-    */
-    public boolean handleTreffer(int stelle){
+
+    /**
+     * Aktualisiere Treffer Array an der jeweiligen Stelle rufe danach check
+     * versenkt auf BSP.:
+     *
+     * [][X][][] -> Stelle 1 getroffen wird mit 1 belegt . . [X][X][X][X] ->
+     * Alle getroffen -> Array durchgehend mit eins belegt -> check versenkt
+     * gibt true zurück
+     *
+     * muss nicht überprüft werden, da nicht zwei mal auf die gleiche Stelle
+     * geschossen werden kann Woher kommt die Stelle wird hier geparsed nach
+     * row/col oder schon woanders?
+     *
+     * @param stelle -> Stelle die ins Array eingetragen wird
+     * @return gibt true für versenkt und false für nicht versenkt zurück so
+     * kann GUI entscheiden
+     */
+    public boolean handleTreffer(int stelle) {
         trefferArray[stelle] = 1;
-        return checkVersenkt(); //gibt true für versenkt und false für nicht versenkt zurück so kann GUI entscheiden
-    }  
+        return checkVersenkt();
+    }
 }
