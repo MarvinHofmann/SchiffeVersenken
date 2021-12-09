7127ede8dd5804f984fa0e7b92e418e734776a0f
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-12-09 11:31:55 +0100

    Kommentare gelöscht

--- SchiffeVersenken/src/Server/Client.java
+++ SchiffeVersenken/src/Server/Client.java
@@ -15,9 +15,7 @@ public class Client {
     
     private int size;
     private int[] schiffe = {0, 0, 0, 0};
-    // Client-Seite eines sehr einfachen Chat-Programms mit Sockets.
-    // (Anstelle von "throws IOException" sollte man Ausnahmen besser
-    // gezielt mit try-catch auffangen.)
+    
     public Client(String ip, SpielGUIController spielGuiController) {
         this.ipAddress = ip;
         this.guiController = spielGuiController;
@@ -25,30 +23,15 @@ public class Client {
     
     public void start(){
         try {
-            // Verwendete Portnummer (vgl. Server).
-	// Verbindung zum Server mit Name oder IP-Adresse args[0]
-	// über Portnummer port herstellen.
-	// Als Resultat erhält man ein Socket.
+        
 	Socket s = new Socket(ipAddress, port);
         System.out.println(ipAddress);
 	System.out.println("Connection established Hier bei Client.");
-	// Ein- und Ausgabestrom des Sockets ermitteln
-	// und als BufferedReader bzw. Writer verpacken
-	// (damit man zeilen- bzw. zeichenweise statt byteweise arbeiten kann).
-	/*BufferedReader in =
-		new BufferedReader(new InputStreamReader(s.getInputStream()));
-	Writer out = new OutputStreamWriter(s.getOutputStream());
-
-	// Standardeingabestrom ebenfalls als BufferedReader verpacken.
-	BufferedReader usr = 
-			new BufferedReader(new InputStreamReader(System.in));
-                        */
+	
         in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         out = new OutputStreamWriter(s.getOutputStream());
         usr = new BufferedReader(new InputStreamReader(System.in));
-	// Abwechselnd vom Benutzer lesen und ins Socket schreiben
-	// bzw. vom Socket lesen und auf den Bildschirm schreiben.
-	// Abbruch bei EOF oder Leerzeile vom Benutzer bzw. bei EOF vom Socket.
+	
 	while (true) {
             
             String line = in.readLine();
@@ -83,19 +66,12 @@ public class Client {
                 }
             }
             
-            
-            
-	    // flush sorgt dafür, dass der Writer garantiert alle Zeichen
-	    // in den unterliegenden Ausgabestrom schreibt.
-
-	    
 	    if (line == null){
                 System.out.println("Line in null");
                 break;
             };
 	}
 
-	// EOF ins Socket "schreiben".
 	s.shutdownOutput();
 	System.out.println("Connection closed.");
         
@@ -116,7 +92,6 @@ public class Client {
     public void analyze(String channel, String value){
         System.out.println("Channel: " + channel);
         System.out.println("Wert: " + value);
-        
         
         switch(channel){
             case "size":
